8097a52d7bb09b7f4b9d54a14370974e5ac4375a
abderrazaqoussi <abderrazaqoussimbarek@gmail.com>
2022-04-14 16:38:39 +0000

    New Update

--- src/Components/UserIcon/DragArea.js
+++ src/Components/UserIcon/DragArea.js
@@ -3,44 +3,94 @@ import { useEffect, useRef, useState } from 'react'
 const DragArea = ({ src }) => {
   const [inAction, setInAction] = useState(false)
   const box = useRef(null)
-  const mousedown = () => {
+
+  //
+  const mousedown = (event) => {
     setInAction(true)
+    const editBox = box.current
+    const boxWidth = editBox.offsetWidth
+    const boxHeight = editBox.offsetHeight
+    const editTool = editBox.childNodes[0]
+    const editToolWidth = editTool.offsetWidth
+    const editToolHeight = editTool.offsetHeight
+    const xPosition = event.pageX - editBox.offsetLeft
+    const yPosition = event.pageY - editBox.offsetTop
+    const cursorX = xPosition - editToolWidth / 2
+    const cursorY = yPosition - editToolHeight / 2
+
+    // console.log([yPosition, cursorY, editToolHeight / 2])
+    //
+    if (cursorX >= 0 && cursorX <= boxWidth - editToolWidth) {
+      editTool.style.left = `${cursorX}px`
+    } else if (cursorX < 0) {
+      editTool.style.left = `${0}px`
+    } else if (cursorX > boxWidth - editToolWidth) {
+      editTool.style.left = `${boxWidth - editToolWidth}px`
+    }
+
+    // console.log([cursorY, 0, editToolHeight, editToolHeight - cursorY])
+    if (cursorY >= 0 && cursorY <= boxHeight - editToolHeight) {
+      editTool.style.top = `${cursorY}px`
+    } else if (cursorY < 0) {
+      editTool.style.top = `${0}px`
+    } else if (cursorY > boxHeight - editToolHeight) {
+      editTool.style.top = `${boxHeight - editToolHeight}px`
+    }
+
+    //
   }
+
   const mousemove = (event) => {
     const editBox = box.current
     const boxWidth = editBox.offsetWidth
+    const boxHeight = editBox.offsetHeight
     const editTool = editBox.childNodes[0]
+    const editToolWidth = editTool.offsetWidth
+    const editToolHeight = editTool.offsetHeight
     const xPosition = event.pageX - editBox.offsetLeft
     const yPosition = event.pageY - editBox.offsetTop
+    const cursorX = xPosition - editToolWidth / 2
+    const cursorY = yPosition //+ editToolHeight / 2
+
     if (!inAction) return
-    if (xPosition > 0 && xPosition < boxWidth) {
-      editTool.style.left = `${xPosition}px`
+    // Drag X
+    if (cursorX >= 0 && cursorX <= boxWidth - editToolWidth) {
+      editTool.style.left = `${cursorX}px`
+    } else if (cursorX < 0) {
+      editTool.style.left = `${0}px`
+    } else if (cursorX > boxWidth - editToolWidth) {
+      editTool.style.left = `${boxWidth - editToolWidth}px`
     }
 
-    editTool.style.top = `${yPosition}px`
+    // Drag Y
+    if (cursorY >= 0 && cursorY <= boxHeight - editToolHeight) {
+      editTool.style.top = `${cursorY}px`
+    } else if (cursorY < 0) {
+      editTool.style.top = `${0}px`
+    } else if (cursorY > boxHeight - editToolHeight) {
+      editTool.style.top = `${boxHeight - editToolHeight}px`
+    }
+
+    // End
   }
   const mouseup = () => {
     setInAction(false)
   }
-  const mouseleave = () => {
-    setInAction(false)
-  }
+
   //
   useEffect(() => {
     // const editBox = box.current
     const editBox = box.current
-    console.log(src)
-    editBox.style.backgroundImage = `url('${src}')`
+    // console.log(src)
+    editBox.style.backgroundImage = `url('./01.png')`
     editBox.addEventListener('mousedown', mousedown)
     editBox.addEventListener('mousemove', mousemove)
     editBox.addEventListener('mouseup', mouseup)
-    editBox.addEventListener('mouseleave', mouseleave)
 
     return () => {
       editBox.removeEventListener('mousedown', mousedown)
       editBox.removeEventListener('mousemove', mousemove)
       editBox.removeEventListener('mouseup', mouseup)
-      editBox.removeEventListener('mouseleave', mouseleave)
     }
   })
   //
@@ -52,8 +102,8 @@ const DragArea = ({ src }) => {
           <div className='dragTool'></div>
         </div>
         <div className='editActions'>
-          <button className='editBtn'>Done</button>
           <button className='editBtn'>Cancel</button>
+          <button className='editBtn'>Done</button>
         </div>
       </div>
     </div>
