f06d581f04c3b0a90f25e4150f5a1387ade67c4a
Ousssssssssi <abderrazaqoussimbarek@gmail.com>
2022-04-25 22:28:39 +0100

    Slider Fixed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import PopMenu from './Components/PopUpMenu/index'
 import DropMenu from './Components/DropDownMenu/index'
 import UserIcon from './Components/UserIcon/index'
 import Table from './Components/RoundedTable/Table'
+import Drag from './Components/DragSlider/Index'
 
 const App = () => {
   return (
     <div>
-      <Table />
+      <Drag />
     </div>
   )
 }
--- src/Components/RoundedTable/Table.js
+++ src/Components/RoundedTable/Table.js
@@ -4,21 +4,43 @@ const Table = () => {
   const plates = useRef(null)
   // (x + r cos(2kπ/n), y + r sin(2kπ/n))
   useEffect(() => {
+    const container = plates.current.parentNode
     const platesChilds = plates.current.childNodes
     const numbreOfPlate = platesChilds.length
+
+    //
+    container.childNodes[0].style.width = `${
+      (50 * container.offsetHeight) / 100
+    }px`
+    container.childNodes[0].style.height = `${
+      (50 * container.offsetHeight) / 100
+    }px`
+
+    //
+    for (let index = 0; index < numbreOfPlate; index++) {
+      platesChilds[index].style.width = `${
+        (20 * container.offsetHeight) / 100
+      }px`
+      platesChilds[index].style.height = `${
+        (20 * container.offsetHeight) / 100
+      }px`
+    }
+  }, [])
+
+  //
+  useEffect(() => {
+    const platesChilds = plates.current.childNodes
+    const numbreOfPlate = platesChilds.length
+    const containerHeight = plates.current.parentNode.offsetHeight
+    const margin = (40 * containerHeight) / 100
     for (let index = 0; index < numbreOfPlate; index++) {
       const x = Math.cos((2 * index * Math.PI) / numbreOfPlate)
       const y = Math.sin((2 * index * Math.PI) / numbreOfPlate)
       console.log(x, y)
-      platesChilds[index].style.transform = `translate(${200 * x}px,${
-        200 * y
+      platesChilds[index].style.transform = `translate(${margin * x}px,${
+        margin * y
       }px)`
     }
-    // platesChilds.forEach((element) => {
-    //   console.log(element, numbreOfPlate)
-    //   // element.style.left = x + 'px'
-    //   // element.style.top = y + 'px'
-    // })
   }, [plates])
 
   return (
